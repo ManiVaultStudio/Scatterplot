8a749cbf6771a3b772c09dde22830721d60f2f46
Thomas Kroes <t.kroes@lumc.nl>
2022-10-30 17:14:07 +0100

    Work on action hierarchy

--- src/ScalarAction.cpp
+++ src/ScalarAction.cpp
@@ -11,7 +11,7 @@ using namespace hdps::gui;
 ScalarAction::ScalarAction(QObject* parent, ScatterplotPlugin* scatterplotPlugin, const QString& title, const float& minimum, const float& maximum, const float& value, const float& defaultValue) :
     PluginAction(parent, scatterplotPlugin, "Scalar"),
     _magnitudeAction(this, title, minimum, maximum, value, defaultValue),
-    _sourceAction(scatterplotPlugin, title)
+    _sourceAction(this, scatterplotPlugin, QString("%1 source").arg(title))
 {
     setText(title);
 
