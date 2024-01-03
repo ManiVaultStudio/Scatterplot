edc56b08b97e081367851103616c9193488bf603
Alexander Vieth <a.vieth@tudelft.nl>
2024-01-03 12:55:43 +0100

    Update dimensions when dataDimensions change (part 2) (#110)
    * Fix edge case
    
    * Handle dataDimensionsChanged like dataChanged
    
    * Regression: for new data sets, always default to first two dimensions

--- src/PositionAction.cpp
+++ src/PositionAction.cpp
@@ -8,7 +8,8 @@ using namespace mv::gui;
 PositionAction::PositionAction(QObject* parent, const QString& title) :
     VerticalGroupAction(parent, title),
     _xDimensionPickerAction(this, "X"),
-    _yDimensionPickerAction(this, "Y")
+    _yDimensionPickerAction(this, "Y"),
+    _dontUpdateScatterplot(false)
 {
     setIcon(mv::Application::getIconFont("FontAwesome").getIcon("ruler-combined"));
     setLabelSizingType(LabelSizingType::Auto);
@@ -25,54 +26,74 @@ PositionAction::PositionAction(QObject* parent, const QString& title) :
         return;
 
     connect(&_xDimensionPickerAction, &DimensionPickerAction::currentDimensionIndexChanged, [this, scatterplotPlugin](const std::uint32_t& currentDimensionIndex) {
+        if (_dontUpdateScatterplot)
+            return;
+
         scatterplotPlugin->setXDimension(currentDimensionIndex);
     });
 
     connect(&_yDimensionPickerAction, &DimensionPickerAction::currentDimensionIndexChanged, [this, scatterplotPlugin](const std::uint32_t& currentDimensionIndex) {
+        if (_dontUpdateScatterplot)
+            return;
+
         scatterplotPlugin->setYDimension(currentDimensionIndex);
-    });
-
-    connect(&scatterplotPlugin->getPositionDataset(), &Dataset<Points>::dataDimensionsChanged, this, [this, scatterplotPlugin]() {
-        // if the new number of dimensions allows it, keep the previous dimension indices
-        auto xDim = _xDimensionPickerAction.getCurrentDimensionIndex();
-        auto yDim = _yDimensionPickerAction.getCurrentDimensionIndex();
-
-        _xDimensionPickerAction.setPointsDataset(scatterplotPlugin->getPositionDataset());
-        _yDimensionPickerAction.setPointsDataset(scatterplotPlugin->getPositionDataset());
-
-        if (static_cast<uint32_t>(xDim) < _xDimensionPickerAction.getNumberOfDimensions())
-            _xDimensionPickerAction.setCurrentDimensionIndex(xDim);
-        else
-            _xDimensionPickerAction.setCurrentDimensionIndex(0);
-
-        if (static_cast<uint32_t>(yDim) < _yDimensionPickerAction.getNumberOfDimensions())
-            _yDimensionPickerAction.setCurrentDimensionIndex(yDim);
-        else
-        {
-            const auto yIndex = _xDimensionPickerAction.getNumberOfDimensions() >= 2 ? 1 : 0;
-
-            _yDimensionPickerAction.setCurrentDimensionIndex(yIndex);
-        }
-
     });
 
     const auto updateReadOnly = [this, scatterplotPlugin]() -> void {
         setEnabled(scatterplotPlugin->getPositionDataset().isValid());
-    };
+        };
 
     updateReadOnly();
 
-    connect(&scatterplotPlugin->getPositionDataset(), &Dataset<Points>::changed, this, [this, scatterplotPlugin, updateReadOnly](mv::DatasetImpl* dataset) {
+    auto onDimensionsChanged = [this, scatterplotPlugin](int32_t xDim, int32_t yDim, auto& currentData) {
+        // Ensure that we never access non-existing dimensions
+        // Data is re-drawn for each setPointsDataset call
+        // with the dimension index of the respective _xDimensionPickerAction set to 0
+        // This prevents the _yDimensionPickerAction dimension being larger than 
+        // the new numDimensions during the first setPointsDataset call
+        _dontUpdateScatterplot = true;
+        _yDimensionPickerAction.setCurrentDimensionIndex(yDim);
+
+        _xDimensionPickerAction.setPointsDataset(currentData);
+        _yDimensionPickerAction.setPointsDataset(currentData);
+
+        // setXDimension() ignores its argument and will update the 
+        // scatterplot with both current dimension indices
+        _xDimensionPickerAction.setCurrentDimensionIndex(xDim);
+        _yDimensionPickerAction.setCurrentDimensionIndex(yDim);
+        scatterplotPlugin->setXDimension(xDim);
+        _dontUpdateScatterplot = false;
+    };
+
+    connect(&scatterplotPlugin->getPositionDataset(), &Dataset<Points>::dataDimensionsChanged, this, [this, updateReadOnly, scatterplotPlugin, onDimensionsChanged]() {
         updateReadOnly();
 
-        _xDimensionPickerAction.setPointsDataset(scatterplotPlugin->getPositionDataset());
-        _yDimensionPickerAction.setPointsDataset(scatterplotPlugin->getPositionDataset());
+        // if the new number of dimensions allows it, keep the previous dimension indices
+        auto xDim = _xDimensionPickerAction.getCurrentDimensionIndex();
+        auto yDim = _yDimensionPickerAction.getCurrentDimensionIndex();
 
-        _xDimensionPickerAction.setCurrentDimensionIndex(0);
+        const auto& currentData = scatterplotPlugin->getPositionDataset();
+        const auto numDimensions = static_cast<int32_t>(currentData->getNumDimensions());
 
-        const auto yIndex = _xDimensionPickerAction.getNumberOfDimensions() >= 2 ? 1 : 0;
+        if (xDim >= numDimensions || yDim >= numDimensions)
+        {
+            xDim = 0;
+            yDim = numDimensions >= 2 ? 1 : 0;
+        }
 
-        _yDimensionPickerAction.setCurrentDimensionIndex(yIndex);
+        onDimensionsChanged(xDim, yDim, currentData);
+    });
+
+    connect(&scatterplotPlugin->getPositionDataset(), &Dataset<Points>::changed, this, [this, scatterplotPlugin, updateReadOnly, onDimensionsChanged]([[maybe_unused]] mv::DatasetImpl* dataset) {
+        updateReadOnly();
+
+        const auto& currentData = scatterplotPlugin->getPositionDataset();
+        const auto numDimensions = static_cast<int32_t>(currentData->getNumDimensions());
+
+        const int32_t xDim = 0;
+        const int32_t yDim = numDimensions >= 2 ? 1 : 0;
+
+        onDimensionsChanged(xDim, yDim, currentData);
     });
 
 }
