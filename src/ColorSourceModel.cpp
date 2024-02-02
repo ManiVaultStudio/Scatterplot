bf27bb80b63664a8064a0fdedd532eda9e7b9835
Thomas Kroes <t.kroes@lumc.nl>
2024-02-02 16:16:52 +0100

    Refactor color source model

--- src/ColorSourceModel.cpp
+++ src/ColorSourceModel.cpp
@@ -6,154 +6,113 @@
 
 using namespace mv;
 
-ColorSourceModel::ColorSourceModel(QObject* parent /*= nullptr*/) :
-    QAbstractListModel(parent),
-    _datasets(),
-    _showFullPathName(true)
+QVariant ColorSourceModel::ConstantColorItem::data(int role /*= Qt::UserRole + 1*/) const
 {
-}
-
-int ColorSourceModel::rowCount(const QModelIndex& parent /*= QModelIndex()*/) const
-{
-    // Constant color option, 2D colormap, plus the number of available datasets
-    return _datasets.count() + 2;
-}
-
-int ColorSourceModel::rowIndex(const Dataset<DatasetImpl>& dataset) const
-{
-    // Only proceed if we have a valid dataset
-    if (!dataset.isValid())
-        return -1;
-
-    if (!_datasets.contains(dataset))
-        return -1;
-
-    // Return the index of the dataset
-    return _datasets.indexOf(dataset) + 2;
-}
-
-int ColorSourceModel::columnCount(const QModelIndex& parent /*= QModelIndex()*/) const
-{
-    return 1;
-}
-
-QVariant ColorSourceModel::data(const QModelIndex& index, int role) const
-{
-    // Get row/column and smart pointer to the dataset
-    const auto row      = index.row();
-    const auto column   = index.column();
-    const auto dataset  = getDataset(row);
-
     switch (role)
     {
-        // Return palette icon for constant color and dataset icon otherwise
         case Qt::DecorationRole:
-            return row > 1 ? dataset->getIcon() : Application::getIconFont("FontAwesome").getIcon("palette");
+            return Application::getIconFont("FontAwesome").getIcon("palette");
 
-        // Return 'Constant' for constant color and dataset (full path) GUI name otherwise
         case Qt::DisplayRole:
-        {
-            if (row > 1)
-            {
-                if (row == 2)
-                    return dataset->text();
-                else
-                    return _showFullPathName ? dataset->getLocation() : dataset->text();
-            }
-            if (row == 1 )
-                return "Scatter layout";
-            else
-                return "Constant";
-        }
+            return "Constant";
 
         default:
             break;
     }
 
-    return QVariant();
+    return {};
 }
 
-void ColorSourceModel::addDataset(const Dataset<DatasetImpl>& dataset)
+QVariant ColorSourceModel::ScatterLayoutItem::data(int role /*= Qt::UserRole + 1*/) const
 {
-    // Insert row into model
-    beginInsertRows(QModelIndex(), rowCount(), rowCount());
+    switch (role)
     {
-        // Add the dataset
-        _datasets << dataset;
+        case Qt::DecorationRole:
+            return Application::getIconFont("FontAwesome").getIcon("palette");
+
+        case Qt::DisplayRole:
+            return "Scatter layout";
+
+        default:
+            break;
     }
-    endInsertRows();
 
-    // Get smart pointer to last added dataset
-    auto& addedDataset = _datasets.last();
+    return {};
+}
 
-    // Remove a dataset from the model when it is about to be deleted
-    connect(&addedDataset, &Dataset<DatasetImpl>::aboutToBeRemoved, this, [this, &addedDataset]() {
-        removeDataset(addedDataset);
+ColorSourceModel::DatasetItem::DatasetItem(Dataset<DatasetImpl> dataset, ColorSourceModel* colorSourceModel) :
+    _dataset(dataset),
+    _colorSourceModel(colorSourceModel)
+{
+    Q_ASSERT(_dataset.isValid());
+    Q_ASSERT(_colorSourceModel);
+
+    if (!dataset.isValid() || !_colorSourceModel)
+        return;
+
+    connect(&_dataset, &Dataset<DatasetImpl>::aboutToBeRemoved, this, [this]() {
+        _colorSourceModel->removeDataset(_dataset);
     });
 
-    // Notify others that the model has updated when the dataset GUI name changes
-    connect(addedDataset.get(), &DatasetImpl::textChanged, this, [this, &addedDataset]() {
+    connect(_dataset.get(), &DatasetImpl::textChanged, this, [this]() {
+        emitDataChanged();
+    });
 
-        // Get row index of the dataset
-        const auto colorDatasetRowIndex = rowIndex(addedDataset);
-
-        // Only proceed if we found a valid row index
-        if (colorDatasetRowIndex < 0)
-            return;
-
-        // Establish model index
-        const auto modelIndex = index(colorDatasetRowIndex, 0);
-
-        // Only proceed if we have a valid model index
-        if (!modelIndex.isValid())
-            return;
-
-        // Notify others that the data changed
-        emit dataChanged(modelIndex, modelIndex);
+    connect(_colorSourceModel, &ColorSourceModel::showFullPathNameChanged, this, [this]() {
+        emitDataChanged();
     });
 }
 
-void ColorSourceModel::removeDataset(const Dataset<DatasetImpl>& dataset)
+QVariant ColorSourceModel::DatasetItem::data(int role /*= Qt::UserRole + 1*/) const
 {
-    // Get row index of the dataset
-    const auto datasetRowIndex = rowIndex(dataset);
-
-    // Remove row from model
-    beginRemoveRows(QModelIndex(), datasetRowIndex, datasetRowIndex);
+    switch (role)
     {
-        // Remove dataset from internal vector
-        _datasets.removeOne(dataset);
+        case Qt::DecorationRole:
+            return _dataset->getIcon();
+
+        case Qt::DisplayRole:
+            return row() == 2 ? _dataset->text() : (_colorSourceModel->getShowFullPathName() ? _dataset->getLocation() : _dataset->text());
+
+        case 200:
+            return _dataset->getId();
+
+        default:
+            break;
     }
-    endRemoveRows();
+
+    return {};
+}
+
+mv::Dataset<mv::DatasetImpl>& ColorSourceModel::DatasetItem::getDataset()
+{
+    return _dataset;
+}
+
+ColorSourceModel::ColorSourceModel(QObject* parent /*= nullptr*/) :
+    QStandardItemModel(parent),
+    _showFullPathName(true)
+{
+    appendRow(new ConstantColorItem());
+    appendRow(new ScatterLayoutItem());
+}
+
+void ColorSourceModel::addDataset(Dataset<DatasetImpl> dataset)
+{
+    appendRow(new DatasetItem(dataset, this));
+}
+
+void ColorSourceModel::removeDataset(Dataset<DatasetImpl> dataset)
+{
+    const auto matches = match(QModelIndex(), 200, dataset->getId(), 1, Qt::MatchExactly);
+
+    if (matches.isEmpty())
+        return;
+
+    removeRows(matches.first().row(), 1);
 }
 
 void ColorSourceModel::removeAllDatasets()
 {
-    // Remove row from model
-    beginRemoveRows(QModelIndex(), 0, rowCount() - 1);
-    {
-        // Remove all datasets
-        _datasets.clear();
-    }
-    endRemoveRows();
-
-    // And update model data with altered datasets
-    updateData();
-}
-
-const Datasets& ColorSourceModel::getDatasets() const
-{
-    return _datasets;
-}
-
-Dataset<DatasetImpl> ColorSourceModel::getDataset(const std::int32_t& rowIndex) const
-{
-    // Return empty smart pointer when out of range
-    if (rowIndex <= 1 || rowIndex > (_datasets.count() + 1))
-        return Dataset<DatasetImpl>();
-
-    // Subtract the constant point size and 2D colormap rows
-    return _datasets[rowIndex - 2];
 }
 
 bool ColorSourceModel::getShowFullPathName() const
@@ -163,24 +122,10 @@ bool ColorSourceModel::getShowFullPathName() const
 
 void ColorSourceModel::setShowFullPathName(const bool& showFullPathName)
 {
+    if (showFullPathName == _showFullPathName)
+        return;
+
     _showFullPathName = showFullPathName;
 
-    updateData();
-}
-
-void ColorSourceModel::updateData()
-{
-    // Update the datasets string list model
-    for (auto dataset : _datasets) {
-
-        // Continue if the dataset is not valid
-        if (!dataset.isValid())
-            continue;
-
-        // Get dataset model index
-        const auto datasetModelIndex = index(_datasets.indexOf(dataset), 0);
-
-        // Notify others that the data changed
-        emit dataChanged(datasetModelIndex, datasetModelIndex);
-    }
+    emit showFullPathNameChanged(_showFullPathName);
 }
