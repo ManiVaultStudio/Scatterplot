eb2eb717c0f691c0dee7ddeb1682f950847d14f6
Thomas Kroes <t.kroes@lumc.nl>
2025-10-08 14:33:03 +0200

    Refactor PDB file handling in conanfile.py
    Refactor PDB file copying logic and ensure all files are copied from the package directory.

--- conanfile.py
+++ conanfile.py
@@ -149,16 +149,19 @@ class ScatterplotOPluginConan(ConanFile):
             ]
         )
         
-        self.copy(pattern="*", src=package_dir)
+        
 
         # Add the pdb files next to the libs for RelWithDebInfo linking
         if self.settings.os == "Windows":
             print("Copying PDBs...")
             pdb_dest = pathlib.Path(package_dir, "RelWithDebInfo", "pdb")
             pdb_dest.mkdir()
-            pdb_files = pathlib.Path(self.build_folder).glob("*.pdb")
+            pdb_files = pdb_files = [p for p in pathlib.Path(self.build_folder).rglob('*') if p.is_file() and p.suffix.lower() == '.pdb']
+            print("PDB(s): ", pdb_files)
             for pfile in pdb_files:
                 shutil.copy(pfile, pdb_dest)
+
+        self.copy(pattern="*", src=package_dir)
 
     def package_info(self):
         self.cpp_info.relwithdebinfo.libdirs = ["RelWithDebInfo/lib"]
