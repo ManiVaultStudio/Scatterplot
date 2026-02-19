69024fcdb615f6433cd9724327e6f3a593ebfc83
Thomas Kroes <t.kroes@lumc.nl>
2026-02-19 11:08:13 +0100

    Feature/Fix gui name rename crash (#230)
    * Refactor scalar source model and update ScalarAction
    
    Convert ScalarSourceModel from QAbstractListModel to QStandardItemModel and introduce typed row items (NameItem, IdItem, Item) and a two-column layout (Name/ID). Datasets are now stored as rows (with helper Row class) and looked up by dataset ID via matching; getDatasets(), getDataset(), add/remove dataset and removeAllDatasets were adapted accordingly. Added getRowIndex() and removed the old per-dataset vector/updateData machinery. Update GUI data handling into Item/NameItem/IdItem (decoration, display and tooltip roles) and connect GUI name changes to emit updates. In ScalarAction: use getRowIndex() instead of rowIndex(), changed setCurrentSourceIndex parameter to std::int32_t, simplified getCurrentDataset() return, and left a TODO-commented dataset connection block. These changes centralize model data in QStandardItem rows, enable multi-column metadata, and simplify dataset lookup/removal by ID.
    
    * Pass model reference to Item/Row
    
    Give Item/NameItem/Row access to their parent ScalarSourceModel by adding a const ScalarSourceModel& parameter and storing it in Item::_scalarSourceModel. Update construction sites (appendRow/Row) to pass *this. Use the model reference in Item::data() to respect getShowFullPathName() and use getGuiName() for display. Add getScalarSourceModel() accessor. Also change getDatasets() to return by value and remove some dead/commented code. These changes allow items to query model state reliably and simplify row construction.

--- src/ScalarSourceModel.cpp
+++ src/ScalarSourceModel.cpp
@@ -8,189 +8,236 @@ using namespace mv;
 using namespace mv::util;
 
 ScalarSourceModel::ScalarSourceModel(QObject* parent /*= nullptr*/) :
-    QAbstractListModel(parent),
+    QStandardItemModel(parent),
     _showFullPathName(true)
 {
+    appendRow(Row(*this, {}));  // Constant source
+    appendRow(Row(*this, {}));  // Selection source
 }
 
-int ScalarSourceModel::rowCount(const QModelIndex& parent /*= QModelIndex()*/) const
+ScalarSourceModel::Item::Item(const ScalarSourceModel& scalarSourceModel, const mv::Dataset<>& scalarDataset) :
+    _scalarSourceModel(scalarSourceModel),
+    _scalarDataset(scalarDataset)
 {
-    // Constant point size option plus the number of available datasets
-    return _datasets.count() + DefaultRow::DatasetStart;
 }
 
-int ScalarSourceModel::rowIndex(const Dataset<DatasetImpl>& dataset) const
+QVariant ScalarSourceModel::Item::data(int role) const
 {
-    // Only proceed if we have a valid dataset
-    if (!dataset.isValid())
-        return -1;
-
-    // Return the index of the dataset and add one for the constant point size option
-    return _datasets.indexOf(dataset) + DefaultRow::DatasetStart;
-}
-
-int ScalarSourceModel::columnCount(const QModelIndex& parent /*= QModelIndex()*/) const
-{
-    return 1;
-}
-
-QVariant ScalarSourceModel::data(const QModelIndex& index, int role) const
-{
-    // Get row/column of and smart pointer to the dataset
-    const auto row              = index.row();
-    const auto column           = index.column();
-    const auto scalarDataset    = getDataset(row);
+    const auto rowIndex = row();
 
     switch (role)
     {
         // Return ruler icon for constant point size and dataset icon otherwise
-        case Qt::DecorationRole:
-        {
-            if (row == DefaultRow::Constant)
-                return StyledIcon("ruler");
+	    case Qt::DecorationRole:
+	    {
+	        if (rowIndex == DefaultRow::Constant)
+	            return StyledIcon("ruler");
 
-            if (row == DefaultRow::Selection)
-                return StyledIcon("mouse-pointer");
+	        if (rowIndex == DefaultRow::Selection)
+	            return StyledIcon("mouse-pointer");
 
-            if (row >= DefaultRow::DatasetStart)
-                return scalarDataset->icon();
+	        if (rowIndex >= DefaultRow::DatasetStart)
+	            return _scalarDataset->icon();
 
-            break;
-        }
+	        break;
+	    }
 
-         // Return 'Constant' for constant point size and dataset (full path) GUI name otherwise
+	    // Return 'Constant' for constant point size and dataset (full path) GUI name otherwise
         case Qt::DisplayRole:
-        {
-            if (row >= DefaultRow::DatasetStart)
-            {
-                if (row == 2)
-                    return scalarDataset->text();
-                else
-                    return _showFullPathName ? scalarDataset->getLocation() : scalarDataset->text();
-            } else {
-                if (row == DefaultRow::Constant)
-                    return "Constant";
+	    {
+		    if (rowIndex >= DefaultRow::DatasetStart)
+		    {
+			    if (rowIndex == 2)
+			    	return _scalarDataset->text();
+			    else
+			        return getScalarSourceModel().getShowFullPathName() ? getScalarDataset()->getLocation() : getScalarDataset()->getGuiName();
+		    }
+		    else {
+			    if (rowIndex == DefaultRow::Constant)
+			    	return "Constant";
 
-                if (row == DefaultRow::Selection)
-                    return "Selection";
-            }
+			    if (rowIndex == DefaultRow::Selection)
+			    	return "Selection";
+		    }
+	    }
 
-            break;
-        }
-
-        default:
-            break;
+	    default:
+	        break;
     }
 
-    return QVariant();
+    return {};
+}
+
+const ScalarSourceModel& ScalarSourceModel::Item::getScalarSourceModel() const
+{
+	return _scalarSourceModel;
+}
+
+const mv::Dataset<>& ScalarSourceModel::Item::getScalarDataset() const
+{
+    return _scalarDataset;
+}
+
+ScalarSourceModel::NameItem::NameItem(const ScalarSourceModel& scalarSourceModel, const mv::Dataset<>& scalarDataset) :
+    Item(scalarSourceModel, scalarDataset)
+{
+	connect(&const_cast<Dataset<>&>(getScalarDataset()), &Dataset<>::guiNameChanged, this, [this]() {
+        emitDataChanged();
+    });
+}
+
+QVariant ScalarSourceModel::NameItem::data(int role) const
+{
+    const auto rowIndex = row();
+
+    switch (role)
+    {
+	    case Qt::DisplayRole:
+	    case Qt::EditRole:
+		{
+            if (rowIndex == DefaultRow::Constant)
+                return "Constant";
+
+            if (rowIndex == DefaultRow::Selection)
+                return "Selection";
+
+            if (rowIndex >= DefaultRow::DatasetStart)
+                return getScalarDataset()->getGuiName();
+
+            break;
+		}
+
+	    case Qt::ToolTipRole:
+        {
+            if (rowIndex == DefaultRow::Constant)
+                return "Constant";
+
+            if (rowIndex == DefaultRow::Selection)
+                return "Selection";
+
+            if (rowIndex >= DefaultRow::DatasetStart)
+                return getScalarDataset()->getLocation();
+
+            break;
+        }
+
+	    default:
+	        break;
+    }
+
+    return Item::data(role);
+}
+
+QVariant ScalarSourceModel::IdItem::data(int role) const
+{
+    const auto rowIndex = row();
+
+    switch (role)
+    {
+	    case Qt::DisplayRole:
+	    case Qt::EditRole:
+	    {
+	        if (rowIndex == DefaultRow::Constant || rowIndex == DefaultRow::Selection)
+	            return "";
+
+	        if (rowIndex >= DefaultRow::DatasetStart)
+	            return getScalarDataset()->getId();
+
+	        break;
+	    }
+
+	    case Qt::ToolTipRole:
+	    {
+            if (rowIndex == DefaultRow::Constant || rowIndex == DefaultRow::Selection)
+                return "";
+
+	        if (rowIndex >= DefaultRow::DatasetStart)
+	            return "ID: " + getScalarDataset()->getId();
+
+	        break;
+	    }
+
+	    default:
+	        break;
+    }
+
+    return Item::data(role);
 }
 
 void ScalarSourceModel::addDataset(const Dataset<DatasetImpl>& dataset)
 {
-    // Avoid duplicates
     if (hasDataset(dataset))
         return;
 
-    // Insert row into model
-    beginInsertRows(QModelIndex(), rowCount(), rowCount());
-    {
-        // Add the dataset
-        _datasets << dataset;
-    }
-    endInsertRows();
-
-    // Get smart pointer to last added dataset
-    auto& addedDataset = _datasets.last();
-
-    // Remove a dataset from the model when it is about to be deleted
-    connect(&addedDataset, &Dataset<DatasetImpl>::aboutToBeRemoved, this, [this, &addedDataset]() {
-        removeDataset(addedDataset);
-    });
-
-    // Notify others that the model has updated when the dataset GUI name changes
-    connect(addedDataset.get(), &DatasetImpl::textChanged, this, [this, &addedDataset]() {
-
-        // Get row index of the dataset
-        const auto colorDatasetRowIndex = rowIndex(addedDataset);
-
-        // Only proceed if we found a valid row index
-        if (colorDatasetRowIndex < 0)
-            return;
-
-        // Establish model index
-        const auto modelIndex = index(colorDatasetRowIndex, 0);
-
-        // Only proceed if we have a valid model index
-        if (!modelIndex.isValid())
-            return;
-
-        // Notify others that the data changed
-        emit dataChanged(modelIndex, modelIndex);
-    });
+    appendRow(Row(*this, dataset));
 }
 
 bool ScalarSourceModel::hasDataset(const Dataset<DatasetImpl>& dataset) const
 {
-    return rowIndex(dataset) >= DefaultRow::DatasetStart;
+    if (!dataset.isValid())
+        return false;
+    else
+		return !match(index(0, static_cast<int>(Column::Id)), Qt::EditRole, dataset->getId(), 1, Qt::MatchExactly).isEmpty();
 }
 
 void ScalarSourceModel::removeDataset(const Dataset<DatasetImpl>& dataset)
 {
-    // Get row index of the dataset
-    const auto datasetRowIndex = rowIndex(dataset);
+    if (!hasDataset(dataset))
+        return;
 
-    // Update model
-    beginRemoveRows(QModelIndex(), datasetRowIndex, datasetRowIndex);
-    {
-        // Remove dataset from internal vector
-        _datasets.removeOne(dataset);
-    }
-    endRemoveRows();
+    const auto matches = match(index(0, static_cast<int>(Column::Id)), Qt::EditRole, dataset->getId(), 1, Qt::MatchExactly);
+
+    if (!matches.isEmpty())
+        removeRow(matches.first().row());
 }
 
 void ScalarSourceModel::removeAllDatasets()
 {
-    // Remove row from model
-    beginRemoveRows(QModelIndex(), 0, rowCount() - DefaultRow::DatasetStart);
-    {
-        // Remove all datasets
-        _datasets.clear();
-    }
-    endRemoveRows();
-
-    // And update model data with altered datasets
-    updateData();
+    removeRows(DefaultRow::DatasetStart, rowCount() - DefaultRow::DatasetStart);
 }
 
-const Datasets& ScalarSourceModel::getDatasets() const
+Datasets ScalarSourceModel::getDatasets() const
 {
-    return _datasets;
+    Datasets datasets;
+
+    for (int rowIndex = DefaultRow::DatasetStart; rowIndex < rowCount(); ++rowIndex)
+    {
+        if (auto item = dynamic_cast<Item*>(itemFromIndex(index(rowIndex, 0)))) {
+            const auto dataset = item->getScalarDataset();
+
+        	if (dataset.isValid())
+                datasets.append(dataset);
+        }
+    }
+
+    return datasets;
 }
 
 Dataset<DatasetImpl> ScalarSourceModel::getDataset(const std::int32_t& rowIndex) const
 {
-    // Return empty smart pointer when out of range
-    if (rowIndex < DefaultRow::DatasetStart || rowIndex > (DefaultRow::DatasetStart + _datasets.count()))
-        return Dataset<DatasetImpl>();
+    if (auto item = dynamic_cast<Item*>(itemFromIndex(index(rowIndex, 0))))
+		return item->getScalarDataset();
 
-    // Subtract the constant point size row
-    return _datasets[rowIndex - DefaultRow::DatasetStart];
+    return {};
 }
 
 void ScalarSourceModel::setDatasets(const Datasets& datasets)
 {
-    // Notify others that the model layout is about to be changed
-    emit layoutAboutToBeChanged();
-
-    // Add datasets
     for (const auto& dataset : datasets)
         addDataset(dataset);
+}
 
-    // Notify others that the model layout is changed
-    emit layoutChanged();
+std::int32_t ScalarSourceModel::getRowIndex(const Dataset<DatasetImpl>& dataset) const
+{
+    if (!dataset.isValid())
+        return -1;
 
-    // And update model data with datasets
-    updateData();
+	const auto matches = match(index(0, static_cast<int>(Column::Id)), Qt::EditRole, dataset->getId(), 1, Qt::MatchExactly);
+
+    if (!matches.isEmpty())
+        return matches.first().row();
+
+    return -1;
 }
 
 bool ScalarSourceModel::getShowFullPathName() const
@@ -201,23 +248,4 @@ bool ScalarSourceModel::getShowFullPathName() const
 void ScalarSourceModel::setShowFullPathName(const bool& showFullPathName)
 {
     _showFullPathName = showFullPathName;
-
-    updateData();
-}
-
-void ScalarSourceModel::updateData()
-{
-    // Update the datasets string list model
-    for (auto dataset : _datasets) {
-
-        // Continue if the dataset is not valid
-        if (!dataset.isValid())
-            continue;
-
-        // Get dataset model index
-        const auto datasetModelIndex = index(_datasets.indexOf(dataset), 0);
-
-        // Notify others that the data changed
-        emit dataChanged(datasetModelIndex, datasetModelIndex);
-    }
 }
